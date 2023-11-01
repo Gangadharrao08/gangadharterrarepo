31239087713866e6b99d4447317c35573b9086aa
Gangadharrao08 <143478731+Gangadharrao08@users.noreply.github.com>
2023-11-01 11:04:58 +0530

    Add files via upload

---
+++ install_apache.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+yum install httpd git -y
+systemctl start httpd
+systemctl status httpd
+cd /var/www/html
+git clone https://github.com/CleverProgrammers/pwj-netflix-clone.git
+mv pwj-netflix-clone/* .
